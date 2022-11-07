e52e6fd77838d8752fe9356256075e04d4f489e0
ssktest <117648493+ssktest@users.noreply.github.com>
2022-11-07 15:17:27 +0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,9 @@
+            <nav>
+                <ul>
+                    <li><a href="#"> Главная </a></li>
+                    <li><a href="#"> Портфолио </a></li>
+                    <li><a href="#"> Проекты </a></li>
+                    <li><a href="#"> Ссылки </a></li>
+                    <li><a href="#"> Контакты </a></li>
+                </ul>    
+            </nav>
